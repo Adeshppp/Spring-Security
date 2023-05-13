2dbaafeff8eea62c5d0078f02f678a381419d0b2
Adesh <adeshp948@gmail.com>
2023-05-13 05:41:25 -0400

    Implemented auth controller

---
+++ src/main/java/com/security/SpringSecurity/dto/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.security.SpringSecurity.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+    public String email;
+    public String password;
+}
