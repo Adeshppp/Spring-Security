9b3d46f837cb1b7257c0de25db916fea146d2b64
Adesh <adeshp948@gmail.com>
2023-05-13 04:45:28 -0400

    Integrating created filer with spring to use.

--- src/main/java/com/security/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/security/SpringSecurity/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.security.SpringSecurity.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+        private final JwtAuthFilter jwtAuthFilter;
 
     //Spring will use below filterchain bean instaed of default one
     @Bean
@@ -17,7 +21,7 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
